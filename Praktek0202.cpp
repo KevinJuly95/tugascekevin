6871c8b181bb10384ccf0436913aa66cda290a20
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2017-10-24 13:47:34 +0700

    Membuat BANYAK Variabel Sekaligus INISIALISASI Lainya

--- Praktek0202.cpp
+++ Praktek0202.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main () {
 	
 	int nilai=0;
 	int Nilai=1;
 	int NILAI=2;
-	int NiLAi=3;
+	int niLAi=3;
+	
+	double angka(10.5),Angka,ANGKA(3.14);
+	int a(10),b(7),c(12);
 	
 }
+
