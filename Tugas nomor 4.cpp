c8390c85d64e8c1f3f8dd197f15d3b87c1d82db0
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2017-11-07 23:06:22 +0700

    tugas nomor 4

---
+++ Tugas nomor 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int nilai;
+	cout<<"Masukkan Nilai	: ";
+	cin>>nilai;
+	if(nilai>0)
+	cout<<"Nilai Positif";
+	else if(nilai<0)
+	cout<<"Nilai Negatif";
+	else
+	cout<<"Nilai 0";
+}
