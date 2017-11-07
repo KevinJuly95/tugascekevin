35805dc081ab74e7503c6a5890ad6ad736dcdf37
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2017-11-07 23:10:19 +0700

    Tugas nomor 5

---
+++ Tugas nomor 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float berat;
+	int tinggi;
+	cout<<"Masukkan Tinggi Badan (cm)	: ";
+	cin>>tinggi;
+	cout<<"Masukkan Beran Badan (kg)	: ";
+	cin>>berat;
+	if(berat<tinggi/2.5){
+		cout<<"Hasil	: Kurus"<<endl;
+	}
+	else if(tinggi/2.5<=berat<=tinggi/2.3){
+		cout<<"Hasil	: Normal"<<endl;
+	}
+	else if(tinggi/2.3<berat){
+		cout<<"Hasil	: Gemuk"<<endl;
+	}
+}
